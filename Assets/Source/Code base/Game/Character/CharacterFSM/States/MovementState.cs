eaf541a77f55c8d533de5ed8a866e229be17d101
Stanislav N. <stanislav_developer@mail.ru>
2024-06-02 03:18:34 +0500

    Update MovementState.cs

--- Assets/Source/Code base/Game/Character/CharacterFSM/States/MovementState.cs
+++ Assets/Source/Code base/Game/Character/CharacterFSM/States/MovementState.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Source.Code_base
@@ -17,21 +18,32 @@ namespace Assets.Source.Code_base
             Data = data;
         }
 
+        protected PlayerInput Input => _character.Input;
+        protected CharacterController Controller => _character.Controller;
+
         public void Enter()
         {
             Debug.Log(GetType());
         }
 
-        public void Exit(){}
-        
+        public void Exit() { }
+
         public void HandleInput()
         {
-            throw new System.NotImplementedException();
+            Data.DeltaInput = ReadInput();
         }
 
         public void Update()
         {
             throw new System.NotImplementedException();
+        }
+
+        private Vector3 ReadInput()
+        {
+            Vector2 delta = Input.Player.Move.ReadValue<Vector2>();
+            Vector3 worldPosition = new(delta.x, 0, delta.y);
+
+            return worldPosition;
         }
     }
 }
