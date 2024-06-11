e420e1066fa4aa48fe424f1a66e71a48128a9dae
Stanislav N. <stanislav_developer@mail.ru>
2024-06-11 10:19:28 +0500

    +

---
+++ Assets/Source/Code base/Configs/EnemyConfig/EnemyConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.Code_base
+{
+    [CreateAssetMenu(fileName ="EnemyConfigs" ,menuName = "Configs/Enemy")]
+    public class EnemyConfig : ScriptableObject
+    {
+        
+    }
+}
